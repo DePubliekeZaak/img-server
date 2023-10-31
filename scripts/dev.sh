6a70af1cc0bec514e8cc96cbdfd8a01b36076839
Joera Mulders <joera@pop-os.localdomain>
2023-10-31 15:03:48 +0100

    adopting nginx

---
+++ scripts/dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f .env ]
+then
+  export $(cat /srv/img-server/docker/.env | xargs)
+fi
+
+
+docker exec -i db1 pg_dump --username "postgres" $1 > /tmp/switch_dump.sql
+# ah ... werkt niet wanneer er een verbinding is via postgrest!
+#docker exec -i db1 psql --username "postgres" -d "img" -c "SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity >
+docker exec -i db1 psql --username "postgres" -d "img" -c "DROP DATABASE IF EXISTS "$DEV" WITH (FORCE);"
+docker exec -i db1 psql --username "postgres" -d "img" -c "CREATE DATABASE "$DEV";"
+docker exec -i db1 psql --username "postgres" $DEV < /tmp/switch_dump.sql
