08697c24fb357bd2e0927ae87da720fbf4f14fd8
Joera Mulders <joera@pop-os.localdomain>
2023-10-31 12:00:37 +0100

    adopting nginx

--- scripts/stage.sh
+++ scripts/stage.sh
@@ -6,9 +6,9 @@ then
 fi
 
 
-docker exec -i db1 pg_dump --username "postgres" $1 > /opt/backups/switch_dump.sql
+docker exec -i db1 pg_dump --username "postgres" $1 > /tmp/switch_dump.sql
 # ah ... werkt niet wanneer er een verbinding is via postgrest!
 #docker exec -i db1 psql --username "postgres" -d "img" -c "SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity >
 docker exec -i db1 psql --username "postgres" -d "img" -c "DROP DATABASE IF EXISTS "$STAGING" WITH (FORCE);"
 docker exec -i db1 psql --username "postgres" -d "img" -c "CREATE DATABASE "$STAGING";"
-docker exec -i db1 psql --username "postgres" $STAGING < /opt/backups/switch_dump.sql
+docker exec -i db1 psql --username "postgres" $STAGING < /tmp/switch_dump.sql
