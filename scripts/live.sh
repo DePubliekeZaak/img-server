2d5bd9bb5a0bc12d0b9917593d5c328f5cc548b1
Joera Mulders <joera@pop-os.localdomain>
2023-10-31 11:54:07 +0100

    adopting nginx

---
+++ scripts/live.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -f .env ]
+then
+  export $(cat /opt/config/docker/.env | xargs)
+fi
+
+
+docker exec -i db1 pg_dump --username "postgres" $STAGING > /opt/backups/switch_dump.sql
+# ah ... werkt niet wanneer er een verbinding is via postgrest!
+#docker exec -i db1 psql --username "postgres" -d "img" -c "SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity >
+docker exec -i db1 psql --username "postgres" -d "img" -c "DROP DATABASE IF EXISTS "$LIVE" WITH (FORCE);"
+docker exec -i db1 psql --username "postgres" -d "img" -c "CREATE DATABASE "$LIVE";"
+docker exec -i db1 psql --username "postgres" $LIVE < /opt/backups/switch_dump.sql
+
---
+++ scripts/stage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f .env ]
+then
+  export $(cat /srv/img-server/docker/.env | xargs)
+fi
+
+
+docker exec -i db1 pg_dump --username "postgres" $1 > /opt/backups/switch_dump.sql
+# ah ... werkt niet wanneer er een verbinding is via postgrest!
+#docker exec -i db1 psql --username "postgres" -d "img" -c "SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity >
+docker exec -i db1 psql --username "postgres" -d "img" -c "DROP DATABASE IF EXISTS "$STAGING" WITH (FORCE);"
+docker exec -i db1 psql --username "postgres" -d "img" -c "CREATE DATABASE "$STAGING";"
+docker exec -i db1 psql --username "postgres" $STAGING < /opt/backups/switch_dump.sql
